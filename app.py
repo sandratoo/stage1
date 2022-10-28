926e6fea8421a19c7e4612d2fbb553403ea25750
sandratoo <toosandraa@gmail.com>
2022-10-28 19:02:47 +0300

    updated api

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-
 from flask import Flask,jsonify
+from flask_restful import Api,Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/get", methods=["GET"])
-def get_all():
-    all = jsonify({
-        "slackUsername": "sandratoo", 
-        "backend": True,
-        "age": 28,
-        "bio": "Hi, my name sandra, a curious and self-starting developer."
-    })
-    
-    return all
+class getall(Resource):
+    def get(self):
+        all = jsonify({
+            "slackUsername": "sandratoo", 
+            "backend": True,
+            "age": 28,
+            "bio": "Hi, my name sandra, a curious and self-starting developer."
+        })
+        
+        return all
+
+api.add_resource(getall,"/get")
 
 if __name__ == "__main__":
     app.run()

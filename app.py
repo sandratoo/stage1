2b2cab7afd9cb62a6796cd4bef372eb8ba0076fa
sandratoo <toosandraa@gmail.com>
2022-10-28 19:41:28 +0300

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify
 from flask_restful import Api,Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 class getall(Resource):
     def get(self):
